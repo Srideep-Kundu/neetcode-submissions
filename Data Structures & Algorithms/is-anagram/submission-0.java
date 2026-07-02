44e5bbdc97e9d73684d3542bf849f87a563af5db
Srideep Kundu <ksrideep007@gmail.com>
2026-07-02 23:10:20 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+
+        if(c1.length != c2.length) return false;
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        for(int i = 0; i < n; i++){
+            if(c1[i] != c2[i]) return false;
+        }
+        return true;
+    }
+}
